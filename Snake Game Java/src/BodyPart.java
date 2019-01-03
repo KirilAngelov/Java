fcbf13ab9bedc2634398503f479ab37290f15930
KirilAngelov <daimonas@abv.bg>
2019-01-03 22:08:11 +0200

    Made Main,BodyPart and GamePanel. More to come.

---
+++ Snake Game Java/src/BodyPart.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+
+public class BodyPart {
+
+	private int xCoordinate,yCoordinate,width,heigth;
+	
+	public BodyPart(int xCoordinate,int yCoordinate,int tileSize)
+	{
+		this.xCoordinate=xCoordinate;
+		this.yCoordinate=yCoordinate;
+		width=tileSize;
+		heigth=tileSize;
+	}
+	public void tick() 
+	{
+		
+	}
+	public void draw(Graphics g) 
+	{
+		
+	}
+}
